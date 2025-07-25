a1e6e4949b8db38e615b5b716fc5a71c3e82494d
corise-cc <support@corise.cc>
2025-07-25 10:18:08 +0900

    Set up Next.js with app directory and initial homepage

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+// app/page.js
+export default function HomePage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>ようこそ TypingMind へ！</h1>
+      <p>これはあなたのトップページです。</p>
+    </main>
+  );
+}
